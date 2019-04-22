a5656e773c94181a45d1d7beabd9150e76b12227
Felipe Marcelus Mantovani <marcelus20felipe@gmail.com>
2019-04-22 04:18:25 +0100

    Added commenting to methods

--- src/controller/Application.java
+++ src/controller/Application.java
@@ -1,11 +1,51 @@
 package controller;
 
+/**
+ * This interface describe the menu options of the Application system
+ */
 public interface Application {
+    /**
+     * Allows user to look for titles
+     */
     void searchForTitles();
+
+    /**
+     * Allows user to look for Customers
+     */
     void searchForCustomers();
+
+    /**
+     * Allows user to look for rents
+     */
+    void searchRents();
+
+    /**
+     * Allows user to add new Titles
+     */
     void addNewTitles();
+
+    /**
+     * Allows user to add new members to the system.
+     */
     void addNewCustomers();
+
+    /**
+     * Allow users to update customers from the system.
+     */
     void updateCustomers();
+
+    /**
+     * Allows user to record a rent to the system.
+     */
     void performARent();
+
+    /**
+     * Allows user to return a rent
+     */
     void performARentReturn();
+
+    /**
+     * the properties of the implemented classes updater.
+     */
+    void updateFields();
 }
