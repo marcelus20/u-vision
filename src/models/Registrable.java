e40bb052157e910e75ee1af3164d6ca6fde26bae
Felipe Marcelus Mantovani <marcelus20felipe@gmail.com>
2019-05-02 16:05:27 +0100

    Commented the Registrable class

--- src/models/Registrable.java
+++ src/models/Registrable.java
@@ -1,18 +1,39 @@
 package models;
 
+/**
+ * The registrable interface forces the classes to forces implemented classes to have id and name fields and gives it
+ * the getters for the fields.
+ *
+ * Also, because it is registrable, it is
+ */
 public interface Registrable {
 
+    /**
+     * Getter for the field
+     * @return
+     */
     Integer getId();
 
+    /**
+     * Setter for the field ID
+     * @param id
+     */
     void setId(Integer id);
 
+    /**
+     * Getter for the field name
+     * @return
+     */
     String getName();
 
+    /**
+     * Setter for the field name.
+     * @param name
+     */
     void setName(String name);
 
-    void registerInstance();
-
-    void updateInstance();
-
-    void incrementInstanceAmount();
+    /**
+     * To record the class to a file
+     */
+    void commitInstance();
 }
