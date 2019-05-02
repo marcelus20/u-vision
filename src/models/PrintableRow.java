c9a737575fd6f027b9516ae89b5f3de25328c902
Felipe Marcelus Mantovani <marcelus20felipe@gmail.com>
2019-05-02 15:53:01 +0100

    Created Printable

---
+++ src/models/PrintableRow.java
@@ -0,0 +1,31 @@
+package models;
+
+/**
+ * This interface is meant to allow subclasses to be printed in the consoled in a not predefined way.
+ *
+ */
+public interface PrintableRow {
+
+    /**
+     * This method is concerned with the array of Strings that stores VALUES of fields of the implemented classes
+     * EG:
+     * {
+     *     name: "Felipe";
+     *     surename "Mantovani";
+     * }
+     * @return {"Felipe", "Mantovani"}
+     */
+    String[] getRowValuesFromFields();
+
+
+    /**
+     * this method is concerned with the array of Strings that stores the NAME of the fields of the implemented classes
+     * EG:
+     * {
+     *      name: "Felipe";
+     *      surename "Mantovani";
+     * }
+     * @return {"name", "Mantovani"}
+     */
+    String[] getHeadersNameFromFields();
+}
